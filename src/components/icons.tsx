2d748e961a74ac070f9ff54ac2f9f863069fb863
JuanchiiBM <jbenitezmingrone@frba.utn.edu.ar>
2024-03-29 13:32:58 -0300

    Agregado de Iconos, modificacion de proyecto con hover (Incompleto)

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,13 +1,15 @@
 'use client'
 
 import '@/css/icons.css'
+import '@/lib/fonticons/fonticons.css'
+import Image from 'next/image';
 import { useRef } from 'react';
 
 const Icons = () => {
     const icons = useRef<HTMLDivElement>(null);
     let xCursor: number;
     let yCursor: number;
-    window.addEventListener('mousemove', (e:MouseEvent) => {
+    window.addEventListener('mousemove', (e: MouseEvent) => {
         xCursor = e.clientX;
         yCursor = e.clientY;
     });
@@ -18,38 +20,45 @@ const Icons = () => {
         const reactIcon = icons.current?.children[0] as HTMLIFrameElement;
         const xReact = reactIcon.offsetLeft;
         const yReact = reactIcon.offsetTop;
-        reactIcon.style.backgroundImage = `radial-gradient(circle at ${xCursor - (xReact ? xReact : 0)}px ${yCursor + yScroll - (yReact ? yReact : 0)}px, rgb(0, 145, 255) 0%, white 30vw)`;
+        reactIcon.style.backgroundImage = `radial-gradient(circle at ${xCursor - (xReact ? xReact : 0)}px ${yCursor + yScroll - (yReact ? yReact : 0)}px, rgb(0, 145, 255) 3vw, white 30vw)`;
         //htmlIcon
         const htmlIcon = icons.current?.children[1] as HTMLIFrameElement;
         const xHtml = htmlIcon.offsetLeft;
         const yHtml = htmlIcon.offsetTop;
-        htmlIcon.style.backgroundImage = `radial-gradient(circle at ${xCursor - (xHtml ? xHtml : 0)}px ${yCursor + yScroll - (yHtml ? yHtml : 0)}px, rgb(255, 69, 0) 0%, white 30vw)`;
-        //cssIcon
-        const cssIcon = icons.current?.children[2] as HTMLIFrameElement;
-        const xCss = cssIcon.offsetLeft;
-        const yCss = cssIcon.offsetTop;
-        cssIcon.style.backgroundImage = `radial-gradient(circle at ${xCursor - (xCss ? xCss : 0)}px ${yCursor + yScroll - (yCss ? yCss : 0)}px, rgb(48, 135, 255) 0%, white 30vw)`;
+        htmlIcon.style.backgroundImage = `radial-gradient(circle at ${xCursor - (xHtml ? xHtml : 0)}px ${yCursor + yScroll - (yHtml ? yHtml : 0)}px, rgb(255, 69, 0) 3vw, white 30vw)`;
         //bootIcon
-        const bootIcon = icons.current?.children[3] as HTMLIFrameElement;
+        const bootIcon = icons.current?.children[2] as HTMLIFrameElement;
         const xBoot = bootIcon.offsetLeft;
         const yBoot = bootIcon.offsetTop;
-        bootIcon.style.backgroundImage = `radial-gradient(circle at ${xCursor - (xBoot ? xBoot : 0)}px ${yCursor + yScroll - (yBoot ? yBoot : 0)}px, rgb(138, 43, 226) 0%, white 30vw)`;
+        bootIcon.style.backgroundImage = `radial-gradient(circle at ${xCursor - (xBoot ? xBoot : 0)}px ${yCursor + yScroll - (yBoot ? yBoot : 0)}px, rgb(138, 43, 226) 3vw, white 30vw)`;
         //sassIcon
-        const sassIcon = icons.current?.children[4] as HTMLIFrameElement;
+        const sassIcon = icons.current?.children[3] as HTMLIFrameElement;
         const xSass = sassIcon.offsetLeft;
         const ySass = sassIcon.offsetTop;
-        sassIcon.style.backgroundImage = `radial-gradient(circle at ${xCursor - (xSass ? xSass : 0)}px ${yCursor + yScroll - (ySass ? ySass : 0)}px, rgb(216, 112, 147) 0%, white 30vw)`;
+        sassIcon.style.backgroundImage = `radial-gradient(circle at ${xCursor - (xSass ? xSass : 0)}px ${yCursor + yScroll - (ySass ? ySass : 0)}px, rgb(216, 112, 147) 3vw, white 30vw)`;
+        //netIcon
+        const netIcon = icons.current?.children[4] as HTMLIFrameElement;
+        const xNet = netIcon.offsetLeft;
+        const yNet = netIcon.offsetTop;
+        netIcon.style.backgroundImage = `radial-gradient(circle at ${xCursor - (xNet ? xNet : 0)}px ${yCursor + yScroll - (yNet ? yNet : 0)}px, rgb(81, 61, 203) 3vw, white 30vw)`;
+        //nextIcon
+        const nextIcon = icons.current?.children[5] as HTMLIFrameElement;
+        const xNext = nextIcon.offsetLeft;
+        const yNext = nextIcon.offsetTop;
+        nextIcon.style.backgroundImage = `radial-gradient(circle at ${xCursor - (xNext ? xNext : 0)}px ${yCursor + yScroll - (yNext ? yNext : 0)}px, rgb(20, 20, 20) 3vw, white 30vw)`;
+
     }
-    window.addEventListener('mousemove',iconsAnimation);
+    window.addEventListener('mousemove', iconsAnimation);
     window.addEventListener('scroll', iconsAnimation);
 
     return (
         <div ref={icons} className='div-tecnologys'>
             <i className="fa-brands fa-react"></i>
             <i className="fa-brands fa-html5"></i>
-            <i className="fa-brands fa-css3"></i>
             <i className="fa-brands fa-bootstrap"></i>
             <i className="fa-brands fa-sass"></i>
+            <i className='icon-NET'></i>
+            <i className='icon-Next'></i>
         </div>
     )
 }
--- src/components/proyects.tsx
+++ src/components/proyects.tsx
@@ -1,18 +1,37 @@
 'use client'
 
 import '@/css/proyects.css'
+import Image from 'next/image';
+import trex from '../../public/img/trex.png'
+import { useRef } from 'react';
 
 const Proyects = () => {
+    const divsImage = useRef<HTMLDivElement>(null)
 
     return (
         <div className='div-proyects-layers'>
-            <div className='div-trucho'></div>
-            <div className='div-trucho'></div>
-            <div className='div-trucho'></div>
-            <div className='div-trucho'></div>
-            <div className='div-trucho'></div>
-            <div className='div-trucho'></div>
-            <div className='div-trucho'></div>
+            <div className='div-trucho' ref={divsImage}>
+                <Image src={trex} layout='fill' objectFit='contain' alt=''></Image>
+                <span>Texto</span>
+            </div>
+            <div className='div-trucho' ref={divsImage}> 
+
+            </div>
+            <div className='div-trucho' ref={divsImage}>
+
+            </div>
+            <div className='div-trucho' ref={divsImage}>
+
+            </div>
+            <div className='div-trucho' ref={divsImage}>
+
+            </div>
+            <div className='div-trucho' ref={divsImage}>
+
+            </div>
+            <div className='div-trucho' ref={divsImage}>
+
+            </div>
         </div>
     )
 }
