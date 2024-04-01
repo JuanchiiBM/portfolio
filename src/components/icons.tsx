70e7ff2e8d5ddcbe76ba436904568217dfd4cd18
JuanchiiBM <jbenitezmingrone@frba.utn.edu.ar>
2024-03-31 22:10:04 -0300

    Nuevos iconos

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -31,18 +31,23 @@ const Icons = () => {
         const xBoot = bootIcon.offsetLeft;
         const yBoot = bootIcon.offsetTop;
         bootIcon.style.backgroundImage = `radial-gradient(circle at ${xCursor - (xBoot ? xBoot : 0)}px ${yCursor + yScroll - (yBoot ? yBoot : 0)}px, rgb(138, 43, 226) 3vw, white 30vw)`;
+        //typeIcon
+        const typeIcon = icons.current?.children[3] as HTMLIFrameElement;
+        const xType = typeIcon.offsetLeft;
+        const yType = typeIcon.offsetTop;
+        typeIcon.style.backgroundImage = `radial-gradient(circle at ${xCursor - (xType ? xType : 0)}px ${yCursor + yScroll - (yType ? yType : 0)}px, rgb(49, 120, 198) 3vw, white 30vw)`;
         //sassIcon
-        const sassIcon = icons.current?.children[3] as HTMLIFrameElement;
+        const sassIcon = icons.current?.children[4] as HTMLIFrameElement;
         const xSass = sassIcon.offsetLeft;
         const ySass = sassIcon.offsetTop;
         sassIcon.style.backgroundImage = `radial-gradient(circle at ${xCursor - (xSass ? xSass : 0)}px ${yCursor + yScroll - (ySass ? ySass : 0)}px, rgb(216, 112, 147) 3vw, white 30vw)`;
         //netIcon
-        const netIcon = icons.current?.children[4] as HTMLIFrameElement;
+        const netIcon = icons.current?.children[5] as HTMLIFrameElement;
         const xNet = netIcon.offsetLeft;
         const yNet = netIcon.offsetTop;
         netIcon.style.backgroundImage = `radial-gradient(circle at ${xCursor - (xNet ? xNet : 0)}px ${yCursor + yScroll - (yNet ? yNet : 0)}px, rgb(81, 61, 203) 3vw, white 30vw)`;
         //nextIcon
-        const nextIcon = icons.current?.children[5] as HTMLIFrameElement;
+        const nextIcon = icons.current?.children[6] as HTMLIFrameElement;
         const xNext = nextIcon.offsetLeft;
         const yNext = nextIcon.offsetTop;
         nextIcon.style.backgroundImage = `radial-gradient(circle at ${xCursor - (xNext ? xNext : 0)}px ${yCursor + yScroll - (yNext ? yNext : 0)}px, rgb(20, 20, 20) 3vw, white 30vw)`;
@@ -56,6 +61,7 @@ const Icons = () => {
             <i className="fa-brands fa-react"></i>
             <i className="fa-brands fa-html5"></i>
             <i className="fa-brands fa-bootstrap"></i>
+            <i className="icon-TypeScript"></i>
             <i className="fa-brands fa-sass"></i>
             <i className='icon-NET'></i>
             <i className='icon-Next'></i>
